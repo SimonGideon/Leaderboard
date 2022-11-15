970dc9822a2a718ff27481cbdae3ff3604889577
SimonGideon <simongideon918@gmail.com>
2022-11-15 19:54:17 +0300

    ADD styling to match wireframe

--- src/components.js
+++ src/components.js
@@ -1,26 +1,28 @@
 const components = () => {
   const element = document.createElement('div');
+  element.setAttribute('id', 'main-body');
+
   element.innerHTML = `
   <h1>Leaderboard</h1>
-    <div>
+    <div id="outline">
       <div>
         <span id="actions">
           <h2>Recent scores</h2>
           <button type="">Refresh</button>
         </span>
-        <span id="dispaly-section">
-          <p>Name: 100</p>
-          <p>Name: 20</p>
-          <p>Name: 50</p>
-          <p>Name: 78</p>
-          <p>Name: 125</p>
-          <p>Name: 77</p>
-          <p>Name: 42</p>
-        </span>
+        <div id="dispaly-section">
+          <li>Name: 100</li>
+          <li>Name: 20</li>
+          <li>Name: 50</li>
+          <li>Name: 78</li>
+          <li>Name: 125</li>
+          <li>Name: 77</li>
+          <li>Name: 42</li>
+        </div>
       </div>
       <div>
         <h2>Add your scores</h2>
-        <form action="">
+        <form action="" id="form">
           <ul>
             <input type="text" name="name" placeholder="Your name">
           </ul>
