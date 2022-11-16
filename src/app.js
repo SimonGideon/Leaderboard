788a67118e3ee663729622f7f85cff6d09fe66f0
SimonGideon <simongideon918@gmail.com>
2022-11-16 20:02:08 +0300

    ADD create a new game

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import components from './components.js';
 // styles
 import './styles/styles.css';
+import fetchAPI from './modules/newGame';
+
+fetchAPI();
 
 document.body.appendChild(components());
---
+++ src/modules/newGame.js
@@ -0,0 +1,18 @@
+const fetchAPI = () => {
+  fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'My cool new game',
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  )
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+export default fetchAPI;
